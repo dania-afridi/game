efcbdc8b4dfaae21340f853a7f16dc43c9109dae
dania-afridi <dania.awais33@gmail.com>
2022-08-25 12:37:55 +0200

    edited computerChoice function in js file

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function getComputerChoice(){
-    console.log("this is computer choice")
+    let num = Math.floor(Math.random()*3)+1
+    if(num === 1){
+        return "Rock"
+    }else if (num === 2){
+        return "Paper"
+    } else if (num === 3){
+        return "Scissors"
+    }
 }
+
+/* console.log(getComputerChoice()) */
